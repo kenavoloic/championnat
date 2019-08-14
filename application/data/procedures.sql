96b25cc5ab43864eb5093179f9d03eb8fedc5357
Loic Kenavo <kenavoloic@gmail.com>
2019-08-14 18:31:14 +0200

    Correction résultats saison 2000-2001 : ASSE Ajout procédures

--- application/data/procedures.sql
+++ application/data/procedures.sql
@@ -10,6 +10,11 @@ DROP PROCEDURE IF EXISTS D1L1Champions;
 DROP PROCEDURE IF EXISTS D1L1Nombreclubs;
 DROP PROCEDURE IF EXISTS D1L1Info;
 
+DROP PROCEDURE IF EXISTS D1L1Relegation;
+DROP PROCEDURE IF EXISTS D1L1Promotion;
+DROP PROCEDURE IF EXISTS D1L1Maintien;
+DROP PROCEDURE IF EXISTS D1L1Test;
+
 -- Retourne la première et la dernière saison de championnat contenus dans la base.
 DELIMITER //
 CREATE PROCEDURE D1L1Minmax()
@@ -19,7 +24,7 @@ END //
 DELIMITER ;
 
 -- Retourne le tableau d’une saison donnée.
--- Prise en compte de l'existence des bonus offensifs et sanctions
+-- Prise en compte de l’existence des bonus offensifs et sanctions
 DELIMITER //
 CREATE PROCEDURE D1L1Saison(IN annee int(4))
 BEGIN
@@ -63,13 +68,13 @@ DELIMITER ;
 -- Retourne la liste de tous les clubs champions
 DELIMITER //
 CREATE PROCEDURE D1L1Champions()
--- Impossible d'utiliser la procédure SAISON en raison des boni offensifs et sanctions.
+-- Impossible d’utiliser la procédure SAISON en raison des boni offensifs et sanctions.
 BEGIN
 select saison, club, equipe_id, victoire, match_nul as nul, defaite, bp, bc, difference_de_buts as diff, bonus_offensif as bonus, sanction, max(points) from resultats group by  saison;
 END //
 DELIMITER ;
 
--- Retourne le club champion pour l'année demandée
+-- Retourne le club champion pour l’année demandée
 DELIMITER //
 CREATE PROCEDURE D1L1Champion(IN annee INT)
 BEGIN
@@ -93,5 +98,45 @@ BEGIN
 CALL champions(annee);
 CALL saison(annee);
 END //
+DELIMITER ;
+
+-- Retourne le nom des équipes reléguées en fin de saison.
+-- Condition n + 1 doit exister. 
+DELIMITER //
+CREATE PROCEDURE d1l1Relegation(in annee INT)
+BEGIN
+select t1.club from (select club from resultats where saison = annee ) t1
+where t1.club not in (select club from resultats where saison = annee + 1);
+END//
+DELIMITER ;
+
+-- Retourne le nom des équipes promues en début de saison
+-- Condition n -1 doit exister.
+DELIMITER //
+CREATE PROCEDURE d1l1Promotion(in annee INT)
+BEGIN
+select t1.club from (select club from resultats where saison = annee ) t1
+where t1.club not in (select club from resultats where saison = annee - 1);
+END//
+DELIMITER ;
+
+-- Retourne le nom des équipes qui se sont maintenues la saison précédente.
+-- Condition n -1 doit exister.
+DELIMITER //
+CREATE PROCEDURE d1l1Maintien(in annee INT)
+BEGIN
+select t1.club from (select club from resultats where saison = annee ) t1
+where t1.club in (select club from resultats where saison = annee - 1);
+END//
+DELIMITER ;
+
+
+
+DELIMITER //
+CREATE PROCEDURE d1l1test(in annee INT)
+BEGIN
+select t1.club from (select club from resultats where saison = annee ) t1
+where t1.club in (select club from resultats where saison = annee - 1);
+END//
 DELIMITER ;
 
